7c529fff7f12befa07d3593c23baa54e73d5ea3c
dogmoore <75594755+dogmoore@users.noreply.github.com>
2020-12-08 13:36:07 -0600

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,16 +2,37 @@ module.exports = {
     name: 'say',
     discription: 'placeholder8',
     execute(message, args) {
-        
-        if (!args.length) {
-            return;
+        let dog = '376857933067321366';
+        const owner = message.guild.owner;
+        const Discord = require('discord.js');
+        let  success = ` `;
+        if (message.author.id === `${dog}` || `${owner}`) {
+
+            if (!args.length) {
+                message.delete();
+                return;
+            }
+            else {
+                let sendMessage = message.content.substring(4);
+                
+                message.channel.send(`${sendMessage}`);
+                message.delete();
+
+                success = `success`;
+            }
         }
         else {
-            let sendMessage = message.content.substring(4);
+            const sayError = new Discord.MessageEmbed()
+                .setColor('#0339fc')
+                .setTitle('Permission Error')
+                .setThumbnail('https://i.imgur.com/8lRaG6L.png')
+                .addField('You do not have Permission to use this command', 'Please go fuck yourself', true)
+                .setTimestamp()
+                .setFooter('APaperBot Created by dogmoore#0001');
 
-            console.log(`Say command used in server: ${message.guild.name}\nIssued by: ${message.author.tag}\n\n`);
-            message.channel.send(`${sendMessage}`);
-            message.delete();
+            message.channel.send(sayError);
+            success = `failure`;
         }
+        console.log(`Say command used in server: ${message.guild.name}\nIssued by: ${message.author.tag}\n${success}\n\n`);
     }
 }
