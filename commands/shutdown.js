2d16293174d82eba19b4746e79e13bcf23b1dd3a
dogmoore <75594755+dogmoore@users.noreply.github.com>
2020-12-08 13:37:03 -0600

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -2,10 +2,25 @@ module.exports = {
     name: 'shutdown',
     discription: 'Placeholder3',
     execute(message) {
-        
-        console.log(`Shutdown command used in server: ${message.guild.name}\nIssued by: ${message.author.tag}\n\n`);
-        console.log('shutting down...');
-        message.reply('You fucking killed me!');
-        setTimeout(() => { process.exit(); }, 2000);
+        const Discord = require('discord.js');
+        const { BotOwner, GuildOwner } = require('../permissions.json');
+        if (message.author.id === `${BotOwner}`) {
+
+            console.log(`Shutdown command used in server: ${message.guild.name}\nIssued by: ${message.author.tag}\n\n`);
+            console.log('shutting down...');
+            message.reply('You fucking killed me!');
+            setTimeout(() => { process.exit(); }, 2000);
+        }
+        else {
+            const shutdownError = new Discord.MessageEmbed()
+                .setColor('#0339fc')
+                .setTitle('Permission Error')
+                .setThumbnail('https://i.imgur.com/8lRaG6L.png')
+                .addField('You do not have Permission to use this command', 'Please go fuck yourself', true)
+                .setTimestamp()
+                .setFooter('APaperBot Created by dogmoore#0001');
+
+            message.channel.send(shutdownError);
+        }
     }
 }
