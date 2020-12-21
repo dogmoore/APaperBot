2084290c7b09384968f4218f77c75e1e93dbd9e2
dogmoore <75594755+dogmoore@users.noreply.github.com>
2020-12-21 12:09:30 -0600

    Update help.js
    attempt at database integration(messy needs a cleanup)

--- commands/help.js
+++ commands/help.js
@@ -1,71 +1,182 @@
 module.exports = {
+
     name: 'help',
-    discription: 'placeholder6',
+
+    discription: 'Labels all known commands based on permission level',
+
     execute(message) {
+
         const Discord = require('discord.js');
-        const { BotOwner } = require('../permissions.json');
+
+        //const { BotOwner } = require('../permissions.json');
+
+        
+
+	const { host, port, user, password, database, charset } = require('../database-info.json');
+
+
+
+	const mysql = require('mysql');
+
+        const connection = mysql.createConnection({
+
+            host: host,
+
+	    port: port,
+
+	    user: user,
+
+	    password: password,
+
+	    database: database,
+
+	    charset: charset
+
+	});
+
+	connection.connect(function(err) {
+
+    connection.query("SELECT * FROM Admin", function(err, result, fields) {
+
+	const results = result;
+
+	});});
+
         const GuildOwner = message.guild.owner;
+
+        if ( results.include(`${GuildOwner}`) ) {
+
+	    console.log(`true`);
+
+	}
+
+        
+
         const { prefix } = require('../config.json');
+
         if (message.author.id === `${BotOwner}`) {
+
             const Help = new Discord.MessageEmbed()
+
                 .setColor('#0339fc')
+
                 .setTitle('Bot commands')
+
                 .setURL('https://bit.ly/2JMYqCD')
+
                 .setThumbnail('https://i.imgur.com/8lRaG6L.png')
+
                 .addFields(
+
                     { name: `${prefix}kick`, value: 'Makes a joke about the bot kicking someone' },
+
                     { name: `${prefix}ban`, value: 'Makes a joke about the bot banning someone' },
+
                     { name: `${prefix}dm`, value: 'Makes a joke about the bot DMing someone' },
+
                     { name: `${prefix}server`, value: 'Used to say the server name and current member count' },
+
                     { name: `${prefix}love_you`, value: 'Makes the bot say `@ user, I Love You`' },
+
                     { name: `\u200B`, value: '\u200B' },
+
                     { name: `${prefix}tkick`, value: 'Actually kick someone from the server!' },
+
                     { name: `${prefix}tban`, value: 'Actually ban someone from the server!' },
+
                     { name: `${prefix}shutdown`, value: 'Force a shutdown' },
+
                 )
+
                 .setTimestamp()
+
                 .setFooter('APaperBot Created by dogmoore#0001');
+
             console.log(`Bot-owner help command used in server: ${message.guild.name}\nIssued by bot owner: ${message.author.tag}\n\n`);
+
             message.channel.send(Help);
+
         }
-        else if (message.author.id === `${GuildOwner}`) {
+
+        else if (message.author.id === `${GuildOwner2}`) {
+
             const Help = new Discord.MessageEmbed()
+
                 .setColor('#0339fc')
+
                 .setTitle('Bot commands')
+
                 .setURL('https://bit.ly/2JMYqCD')
+
                 .setThumbnail('https://i.imgur.com/8lRaG6L.png')
+
                 .addFields(
+
                     { name: `${prefix}kick`, value: 'Makes a joke about the bot kicking someone' },
+
                     { name: `${prefix}ban`, value: 'Makes a joke about the bot banning someone' },
+
                     { name: `${prefix}dm`, value: 'Makes a joke about the bot DMing someone' },
+
                     { name: `${prefix}server`, value: 'Used to say the server name and current member count' },
+
                     { name: `${prefix}love_you`, value: 'Makes the bot say `@ user, I Love You`' },
+
                     { name: '\u200B', value: '\u200B' },
+
                     { name: `${prefix}tkick`, value: 'Actually kick someone from the server!' },
+
                     { name: `${prefix}tban`, value: 'Actually ban someone from the server!' },
+
                 )
+
                 .setTimestamp()
+
                 .setFooter('APaperBot Created by dogmoore#0001');
+
             console.log(`Guild-owner help command used in server: ${message.guild.name}\nIssued by owner: ${message.author.tag}\n\n`);
+
             message.channel.send(Help);
+
         }
+
         else {
+
             const Help = new Discord.MessageEmbed()
+
                 .setColor('#0339fc')
+
                 .setTitle('Bot commands')
+
                 .setURL('https://bit.ly/2JMYqCD')
+
                 .setThumbnail('https://i.imgur.com/8lRaG6L.png')
+
                 .addFields(
+
                     { name: `${prefix}kick`, value: 'Ping someone to kick them' },
+
                     { name: `${prefix}ban`, value: 'Ping someone to ban them' },
+
                     { name: `${prefix}dm`, value: 'Ping someone to DM them' },
+
                     { name: `${prefix}server`, value: 'Used to say the server name and current member count' },
+
                     { name: `${prefix}love_you`, value: 'To find out if I love you' },
+
                 )
+
                 .setTimestamp()
+
                 .setFooter('APaperBot Created by dogmoore#0001');
+
             console.log(`Help command used in server: ${message.guild.name}\nIssued by: ${message.author.tag}\n\n`);
+
             message.channel.send(Help);
+
         }
+
     }
+
 }
+
