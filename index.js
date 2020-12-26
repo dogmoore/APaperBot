6a1a773e7b788cc19e27346a8292310981e46427
dogmoore <75594755+dogmoore@users.noreply.github.com>
2020-12-26 02:03:27 -0600

    Update index.js
    cleaned up prefix commands and added non-prefix commands

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
+const { token } = require('./token.json');
 const emojiRegex = require('emoji-regex/RGI_Emoji.js');
 const regex = emojiRegex();
 const { BotOwner, GuildOwner } = require('./permissions.json');
-
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const noPrefixCommands = fs.readdirSync('./non-prefix commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
+function connecting() {
+    console.log('APaperBot is connecting to Discord...');
+    setTimeout(connected, 1500);
+}
+function connected() {
+    console.log('APaperBot is now connected to Discord');
+}
+
 client.once('ready', () => {
-  console.log('***APaperBot is active!***');
-    client.user.setActivity(`paper sleep!`, { type: 'WATCHING', status: 'idle' });
+    setTimeout(connecting, 500);
+    client.user.setActivity(`with Paper's emotions`, { type: 'STREAMING', status: 'active' });
 });
 
 for (const file of commandFiles) {
@@ -25,73 +34,87 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
-
-
 client.on('message', message => {
-    let emote = "<:face_with_raised_eyebrow:786381795838656553>";
-    let mee6 = message.channel.content;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);//gets the arguments of a command
     try {
-        if (mee6 === regex.exec(emote)) {
-            message.channel.send("<:face_with_raised_eyebrow:786381795838656553>");
-            console.log('worked');
+
+        if (message.author.id === '159985870458322944') {
+            client.commands.get('mee6').execute(message, args);
+        }
+        if (message.author.bot) return;//creates the bot removal
+        if (message.mentions.has(client.user)) {
+            client.commands.get('selfping').execute(message, args);
+            return;
+        }
+        if (message.content.includes('jellyfish') || message.content.includes('Jellyfish')) {
+            cliet.commands.get('jellyfish').execute(message, args);
+        }
+        if (message.author.id === '278548721778688010' || message.author.id === '410454488885755916') {
+            client.commands.get('jelly').execute(message, args);
+        }
+        if (message.content.includes('waffle')) {
+            client.commands.get('waffle').execute(message, args);
         }
     }
     catch (err) {
-        console.log('failed');
-    }
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-    const command = client.commands.get(commandName);
-    const { AllRoles } = require('./permissions.js');
-
-    try {
-        if (commandName === 'love-you') {
-            command.execute(message);
-        }
-
-        //start testing command
-        else if (commandName === 'testing') {
-            command.execute(message, args);
-        }
-        //end testing command
-
-        else if (commandName === 'say') {
-            command.execute(message, args);
-        }
-
-        else if (commandName === 'dm') {
-            command.execute(message);
-        }
-
-        else if (commandName === 'kick') {
-            command.execute(message);
-        }
-
-        else if (commandName === 'shutdown') {
-            command.execute(message);
-        }
-
-        else if (commandName === 'server') {
-            command.execute(message);
-        }
-
-        else if (commandName === 'help') {
-            command.execute(message);
-            //console.log(`${AllRoles}`);
-        }
-        else if (commandName === 'tkick') {
-            command.execute(message, args);
-        }
-        else if (commandName === 'tban') {
-            command.execute(message, args);
-        }
-    }
-    catch(err) {
         console.error(err);
     }
     finally {
-        
+
+    }
+
+    if (!message.content.startsWith(prefix)) return;//creates the prefix demand PREFIX FOUND IN CONFIG
+
+    const commandName = args.shift().toLowerCase();//foces commands to lowercase
+    const command = client.commands.get(commandName);//declares commandName
+
+    try {
+        switch (commandName) {
+            case "love-you":
+                command.execute(message);
+                break;
+            case "testing"://TESTING COMMAND//
+                command.execute(message);
+                break;
+            case "say":
+                command.execute(message, args);
+                break;
+            case "dm":
+                command.execute(message);
+                break;
+            case "kick":
+                command.execute(message);
+                break;
+            case "ban":
+                command.execute(message);
+                break;
+            case "help":
+                command.execute(message);
+                break;
+            case "restart":
+                command.execute(message);
+                break;
+
+            //MODERATION COMMANDS//
+            case "shutdown"://BOT OWNER ONLY//
+                command.execute(message, args);
+                break;
+            case "tkick":
+                command.execute(message, args);
+                break;
+            case "tban":
+                command.execute(message, args);
+                break;
+            case "tsay":
+                command.execute(message, args);
+                break;
+        }
+    }
+    catch (err) {
+        console.error(err);
+    }
+    finally {
+
     }
 });
 
