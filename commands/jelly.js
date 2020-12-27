8af626d8d2de1d7b72954bdeb30332b1aff86c0f
dogmoore <75594755+dogmoore@users.noreply.github.com>
2020-12-26 19:56:41 -0600

    Update jelly.js
    added a random num gen for responses(broke)

--- commands/jelly.js
+++ commands/jelly.js
@@ -5,7 +5,31 @@ module.exports = {
         const Discord = require('discord.js');
         try {
             if (message.content.includes('>:D')) {
-                message.channel.send('Jelly be nice now');
+                try {
+                    let x = 4;
+                    function getRandomInt(max) {
+                        return Math.floor(Math.random() * Math.floor(max));
+                    }
+                    if (getRandomInt(x) == 0) return;
+                    else if (getRandomInt(x) == 1) {
+                        message.channel.send('Jelly be nice now');
+                    }
+                    else if (getRandomInt(x) == 2) {
+                        message.channel.send('>:D');
+                    }
+                    else if (getRandomInt(x) == 3) {
+                        message.channel.send('feck off jelly');
+                    }
+                    else {
+                        message.channel.send('fuck');
+                    }
+                }
+                catch (err) {
+                    console.error(err);
+                }
+                finally {
+
+                }
             }
         }
         catch (err) {
