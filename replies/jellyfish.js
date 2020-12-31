cfe323af2aea95b0b56af7bfbf067b1d2a2f7f37
dogmoore <dogmoore024@gmail.com>
2020-12-30 23:29:44 -0600

    fixed jellyfish spam

--- replies/jellyfish.js
+++ replies/jellyfish.js
@@ -1,6 +1,9 @@
 module.exports = (client, message) => {
   const Discord = require('discord.js');
+  const { prefix } = require('../config.json');
   try {
+    if (message.author.bot) return;
+    if(message.content.startsWith(`${prefix}`)) return;
     if (message.content.includes('jellyfish') || message.content.includes('Jellyfish')) {
       message.channel.send('go on and bring that smelly bot here, I\'ll fight it and win!\nFUCK JELLYFISH');
       console.log('jellyfish was mentioned\n\n');
@@ -10,6 +13,6 @@ module.exports = (client, message) => {
     console.error(err);
   }
   finally {
-    
+
   }
 }
