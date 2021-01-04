66dd5e099811c9e590688cfa325d4eeec86b2cc5
dogmoore <dogmoore024@gmail.com>
2021-01-04 16:17:44 -0600

    ignores bot prefix commands

--- replies/swear filter.js
+++ replies/swear filter.js
@@ -5,7 +5,7 @@ module.exports = (client, message) => {
     let i = message.content.toLowerCase();
     if (message.author.bot) return;
     if(message.content.startsWith(`${prefix}`)) return;
-    if(message.content.startsWith('bb')) return;
+    if(message.content.startsWith('bb') || message.content.startsWith('d!') || message.content.startsWith('?') || message.content.startsWith('!')) return;
     else if (i.includes("fuck")||i.includes("sex")||i.includes("damn")||i.includes("hell")||i.includes("shut up")||i.includes("cunt")||i.includes("pussy")||i.includes("asshole")||i.includes("ass")) {
       message.reply('Please watch your fucking language')
     }
