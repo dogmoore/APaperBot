30777d3865cdc1599def4eaf3ec2c944610ec855
dogmoore <dogmoore024@gmail.com>
2021-01-04 16:18:29 -0600

    first attempt at ignoring 'swears' in words

--- replies/swear filter.js
+++ replies/swear filter.js
@@ -5,8 +5,8 @@ module.exports = (client, message) => {
     let i = message.content.toLowerCase();
     if (message.author.bot) return;
     if(message.content.startsWith(`${prefix}`)) return;
-    if(message.content.startsWith('bb') || message.content.startsWith('d!') || message.content.startsWith('?') || message.content.startsWith('!')) return;
-    else if (i.includes("fuck")||i.includes("sex")||i.includes("damn")||i.includes("hell")||i.includes("shut up")||i.includes("cunt")||i.includes("pussy")||i.includes("asshole")||i.includes("ass")) {
+    if(message.content.startsWith('bb') || message.content.startsWith('d!') || message.content.startsWith('?') || message.content.startsWith('!') || message.content.startsWith('-')) return;
+    else if (i.includes(" fuck")||i.includes(" sex")||i.includes(" damn")||i.includes(" hell")||i.includes(" shut up")||i.includes(" cunt")||i.includes(" pussy")||i.includes(" asshole")||i.includes("ass")) {
       message.reply('Please watch your fucking language')
     }
   }
