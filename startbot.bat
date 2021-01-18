0503d6c20e54f8a402f4589cd936a1fe8f829f70
dogmoore <dogmoore024@gmail.com>
2021-01-18 02:14:32 -0600

    created startup file to auto fetch files

---
+++ startbot.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+TITLE APaperBot start-up
+ECHO Fetching files from github, please wait a moment
+GIT fetch https://github.com/dogmoore/APaperBot
+ECHO files fetched, now starting up bot
+NODE index.js
