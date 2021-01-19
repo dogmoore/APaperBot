be28b10aa596958a542096efb23a9421798a4397
dogmoore <dogmoore024@gmail.com>
2021-01-19 16:48:42 -0600

    another attempt at logs

--- index.js
+++ index.js
@@ -14,34 +14,39 @@ client.commands = new Discord.Collection();
 var dashboard = "";
 dashboard = new botdash.APIclient(dashboardtoken);
 
+color.setTheme({
+  logColor: 'magenta bold italic',
+  errorColor: 'red'
+});
+
 let a = 1
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
         const event = require(`./events/${file}`);
         let eventName = file.split(".")[0];
-        console.log(a + `: Attempting to load the event '${eventName}'`.blue.bold);
+        console.log(a + `: Attempting to load the event '${eventName}'`.logColor);
         a = a + 1;
         client.on(eventName, event.bind(null, client));
     });
-    console.log('Successfully loaded all events.'.blue.bold);
+    console.log('Successfully loaded all events.'.logColor);
     eventLoad = 'All good';
 });
 
 fs.readdir("./commands/", (err, files) => {
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if (jsfile.length <= 0) {
-        console.log("Couldn't find commands!".red);
+        console.log("Couldn't find commands!".errorColor);
         cmd = 'Something went wrong';
         return;
     };
     jsfile.forEach((f, i) => {
         let props = require(`./commands/${f}`)
-        console.log(a + `: Attempting to load the command '${props.name}'`.blue.bold)
+        console.log(a + `: Attempting to load the command '${props.name}'`.logColor)
         client.commands.set(props.name, props);
         a = a + 1;
     });
-    console.log("Successfully loaded all commands.".blue.bold);
+    console.log("Successfully loaded all commands.".logColor);
     cmd = 'All good';
 });
 
@@ -50,11 +55,11 @@ fs.readdir("./replies/", (err, files) => {
     files.forEach(file => {
         const replies = require(`./replies/${file}`);
         let eventName = file.split(".")[0];
-        console.log(a + `: Attempting to load the event '${eventName}'`.blue.bold);
+        console.log(a + `: Attempting to load the event '${eventName}'`.logColor);
         a = a + 1;
         client.on("message", replies.bind(null, client));
     });
-    console.log("Successfully loaded all replies".blue.bold);
+    console.log("Successfully loaded all replies".logColor);
     replyLoad = "All good";
 });
 
