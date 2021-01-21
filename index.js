40addea510d666590ef581e8e44ee179d0355767
dogmoore <dogmoore024@gmail.com>
2021-01-21 02:44:08 -0600

    added console based commands

--- index.js
+++ index.js
@@ -1,64 +1,102 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { token } = require('./token.json');
-const { dashboardtoken } = require('./dashboard.json');
 const client = new Discord.Client();
-const { Console } = require('console');
+//const { Console } = require('console');
 const Logger = require('leekslazylogger');
 const log = new Logger;
+const eventEmitter = require('events');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 var commandLoad;
 var eventLoad;
 var replyLoad;
 client.commands = new Discord.Collection();
 
-let a = 1
-fs.readdir("./events/", (err, files) => {
+function consoleCommands() {
+  rl.question('>>>$', (i) => {
+    let x = i.toLowerCase();
+    if(x.startsWith('say')) {
+      let args = x.slice().trim().split(' ');
+      //message.channel.send(say);
+      consoleCommands();
+    }
+    else if(x.startsWith('shutdown')) {
+      log.console(log.format(`&aVIA CONSOLE\n&6APaperBot is shutting down...`));
+      process.exit();
+    }
+    else if(x.startsWith('restart')) {
+      log.console(log.format(`&aVIA CONSOLE\n&6APaperBot is Restarting...`));
+      client.destroy();
+      //emitter.setMaxListeners(0); //0 means infinite
+      client.login(token);
+      log.console(log.format('&6APaperBot has restarted!'));
+      fileCheck();
+      consoleCommands();
+    }
+    else if(x.startsWith('help')) {
+      log.console(log.format('&aVIA CONSOLE\n\n&bConsole Help Commands\nSay [&fCHANNEL ID&b] [&fMESSAGE&b]\nShutdown\nRestart'));
+      consoleCommands();
+    }
+    else {
+      consoleCommands();
+    }
+  })
+}
+
+function fileCheck() {
+  let a = 1
+  fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
-        const event = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        log.console(log.format(`&a${a}: Attempting to load the event '&5${eventName}&a'`));
-        a = a + 1;
-        client.on(eventName, event.bind(null, client));
+      const event = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+      log.console(log.format(`&a${a}: Attempting to load the event '&5${eventName}&a'`));
+      a = a + 1;
+      client.on(eventName, event.bind(null, client));
     });
     log.console(log.format('&6Successfully loaded all events.'));
     eventLoad = 'All good';
-});
 
-fs.readdir("./commands/", (err, files) => {
+  });
+
+  fs.readdir("./commands/", (err, files) => {
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if (jsfile.length <= 0) {
-        log.console(log.format("&cCouldn't find commands!"));
-        cmd = 'Something went wrong';
-        return;
+      log.console(log.format("&cCouldn't find commands!"));
+      cmd = 'Something went wrong';
+      return;
     };
     jsfile.forEach((f, i) => {
-        let props = require(`./commands/${f}`)
-        log.console(log.format(`&a${a}: Attempting to load the command '&5${props.name}&a'`));
-        client.commands.set(props.name, props);
-        a = a + 1;
+      let props = require(`./commands/${f}`)
+      log.console(log.format(`&a${a}: Attempting to load the command '&5${props.name}&a'`));
+      client.commands.set(props.name, props);
+      a = a + 1;
     });
     log.console(log.format("&6Successfully loaded all commands."))
     cmd = 'All good';
-});
 
-fs.readdir("./replies/", (err, files) => {
+  });
+
+  fs.readdir("./replies/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
-        const replies = require(`./replies/${file}`);
-        let eventName = file.split(".")[0];
-        log.console(log.format(`&a${a}: Attempting to load the event '&5${eventName}&a'`));
-        a = a + 1;
-        client.on("message", replies.bind(null, client));
+      const replies = require(`./replies/${file}`);
+      let eventName = file.split(".")[0];
+      log.console(log.format(`&a${a}: Attempting to load the event '&5${eventName}&a'`));
+      a = a + 1;
+      client.on("message", replies.bind(null, client));
     });
     log.console(log.format("&6Successfully loaded all replies"));
     replyLoad = "All good";
-});
 
-let y = process.openStdin()
-y.addListener("data", res => {
-    let x = res.toString().trim().split(/ +/g)
-    client.channels.fetch("759170742154690560").channel.send(x.join(" "));
-});
+  });
+}
+fileCheck();
+
+consoleCommands();
 
 client.login(token);
