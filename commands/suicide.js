305366a405fb25aeaab95f10d93e2ecfb0c2f93d
dogmoore <dogmoore024@gmail.com>
2021-01-23 06:09:39 -0600

    fff

--- commands/suicide.js
+++ commands/suicide.js
@@ -10,48 +10,52 @@ module.exports = {
     function getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max) + 0);
     }
-    if (getRandomInt(x) == 0) {
-      message.reply('was too gay for their own good and exploded');
+
+    function suicide() {
+      if (getRandomInt(x) == 0) {
+        message.reply('was too gay for their own good and exploded');
+      }
+      else if (getRandomInt(x) == 1) {
+        message.reply('got on Jelly\'s bad side');
+      }
+      else if (getRandomInt(x) == 2) {
+        message.reply('got shot and died of lead poisoning');
+      }
+      else if (getRandomInt(x) == 3) {
+        message.reply('tried flirting with Paper around Piper');
+      }
+      else if (getRandomInt(x) == 4) {
+        message.reply('trolled life');
+      }
+      else if (getRandomInt(x) == 5) {
+        message.reply('greatly misunderstood the term *choker*');
+      }
+      else if (getRandomInt(x) == 6) {
+        message.reply('pressed *ALT F4*');
+      }
+      else if (getRandomInt(x) == 7) {
+        message.reply('used the forbidden bathbomb');
+      }
+      else if (getRandomInt(x) == 8) {
+        message.reply('went to meet they\'re savior, Satan');
+      }
+      else if (getRandomInt(x) == 9) {
+        message.reply('insulted Fatal');
+      }
+      else if (getRandomInt(x) == 10) {
+        message.reply('tried to spill government secrets');
+      }
+      else if (getRandomInt(x) == 11) {
+        message.reply('didn\'t try to commit suicide in they\'re cell after running a pedo ring for the top 1%');
+      }
+      else if (getRandomInt(x) == 12) {
+        message.reply('got tickled by Jelly');
+      }
+      else {
+        suicide();
+      }
     }
-    else if (getRandomInt(x) == 1) {
-      message.reply('got on Jelly\s bad side');
-    }
-    else if (getRandomInt(x) == 2) {
-      message.reply('got shot and died of lead poisoning');
-    }
-    else if (getRandomInt(x) == 3) {
-      message.reply('tried flirting with Paper around Piper');
-    }
-    else if (getRandomInt(x) == 4) {
-      message.reply('trolled life');
-    }
-    else if (getRandomInt(x) == 5) {
-      message.reply('greatly misunderstood the term *choker*');
-    }
-    else if (getRandomInt(x) == 6) {
-      message.reply('pressed *ALT F4*');
-    }
-    else if (getRandomInt(x) == 7) {
-      message.reply('used the forbidden bathbomb');
-    }
-    else if (getRandomInt(x) == 8) {
-      message.reply('went to meet they\'re savior, Satan');
-    }
-    else if (getRandomInt(x) == 9) {
-      message.reply('insulted Fatal');
-    }
-    else if (getRandomInt(x) == 10) {
-      message.reply('tried to spill government secrets');
-    }
-    else if (getRandomInt(x) == 11) {
-      message.reply('didn\'t commit suicide in they\'re cell after running the top 1% pedo ring');
-    }
-    else if (getRandomInt(x) == 12) {
-      message.reply('got tickled by Jelly');
-    }
-    else {
-      message.reply('met ched\'s parents');
-    }
+    suicide();
     log.console(log.format(`&bSuicide command used in server: ${message.guild.name}\nIssued by: ${message.author.tag}\n`));
   }
 }
